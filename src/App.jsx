6c8124251b5128f19ffb3c7373f26267ef555b40
Vikash Pandey <44VikashmPandey@gmail.com>
2025-03-15 07:31:03 +0530

    Add code spliting technique for better performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,33 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './components/navbar'
-import Footer from './components/footer'
-import Home from './pages/home'
-import AppRouting from './routers/routes'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import Layout from './Layouts/layout'
-import Contactus from './pages/contactus'
-import AllJewelleryList from './pages/Jewellery'
+
+import { BrowserRouter, HashRouter, Route, Router, Routes } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Layout = React.lazy(() => import("./Layouts/layout"));
+  const Home = React.lazy(() => import("./pages/home/"));
+  const AllJewelleryList = React.lazy(() => import("./pages/Jewellery"));
+  const Contactus = React.lazy(() => import("./pages/contactus"));
 
   return (
     <>
 
-      {/* <BrowserRouter> */}
-        {/* <div>
-          <div className=''>
-            <NavBar />
-
-
-          </div>
-          <div>
-            <AppRouting />
-          </div>
-          <div className='mt-2'>
-            <Layout/>
-          <Footer />
-          </div>
-        </div> */}
-
-{/* <AppRouting /> */}
-       
-      {/* </BrowserRouter> */}
-      <BrowserRouter>
+      
+      <HashRouter>
+      <Suspense fallback={<div>Loading..</div>}>
       <Routes>
-        {/* All routes wrapped in Layout */}
         <Route path="/" element={<Layout />}>
           <Route  index element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="contactus" element={<Contactus />} />
-          {/* path: '/:name/Products',
-      component : <AllJewelleryList/> */}
       <Route path=':name/Products' element={<AllJewelleryList/>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+      </Suspense>
+    </HashRouter>
     </>
   )
 }
